eede8025a6719209072fe790b7aec4ad251541f4
soumu129 <74505201+soumu129@users.noreply.github.com>
2022-11-10 12:02:23 +0900

    test

--- host/startup.bat
+++ host/startup.bat
@@ -3,9 +3,9 @@ set stream_ip=iseifu-chime-sv.local
 set stream_key=app
 set VDexe=\Github\Virtualdesktop\Virtualdesktop.exe
 start vlc --play-and-exit srt://%stream_ip%:1935?streamid=output/live/%stream_key%
-timeout /nobreak 5 < nul
+ping -n 5 localhost < nul
 start %VDexe% -Remove:VLC
-timeout /nobreak 1 < nul
+ping -n 3 localhost < nul
 start %VDexe% -New -Name:VLC
-timeout /nobreak 1 < nul
+ping -n 3 localhost < nul
 start %VDexe% -Getdesktop:VLC -MoveWindowHandle:VLC
