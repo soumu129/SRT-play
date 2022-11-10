f6e0c6bf8e231edaaa8c883f0cb66635bfa8f4ae
soumu129 <74505201+soumu129@users.noreply.github.com>
2022-11-10 11:13:24 +0900

    wincmd update

--- host/setup.bat
+++ host/setup.bat
@@ -5,4 +5,4 @@ winget install VideoLAN.VLC
 git clone https://github.com/MScholtes/VirtualDesktop.git \Github\VirtualDesktop
 \Github\VirtualDesktop\Compile.bat
 
-copy \Github\chime-play\host\startup.bat "%HOMEPATH%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\chime-play.bat"
+copy "\Github\chime-play\host\startup.bat" "%HOMEPATH%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\chime-play.bat"
