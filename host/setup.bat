d8e1b257c5347695997a887e221cea4e76c114e4
soumu129 <74505201+soumu129@users.noreply.github.com>
2022-11-10 11:41:21 +0900

    test

--- host/setup.bat
+++ host/setup.bat
@@ -3,5 +3,5 @@ winget install Git.Git
 winget install VideoLAN.VLC
 copy "\Github\chime-play\host\startup.bat" "%HOMEPATH%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\chime-play.bat"
 git clone https://github.com/MScholtes/VirtualDesktop.git \Github\VirtualDesktop
-\Github\VirtualDesktop\Compile.bat
-
+cd \Github\VirtualDesktop
+Compile.bat
--- host/startup.bat
+++ host/startup.bat
@@ -1,8 +1,8 @@
 @echo off
-set stream_ip = iseifu-chime-sv.local
-set stream_key = app
-set VDexe = \Github\Virtualdesktop\Virtualdesktop.exe
-vlc -I null --play-and-exit srt://%stream_ip%:1935?streamid=output/live/%stream_key%
-%VDexe% -Remove:VLC
-%VDexe% -New -Name:VLC
-%VDexe% -Getdesktop:VLC -MoveWindowHandle:VLC
+set stream_ip=iseifu-chime-sv.local
+set stream_key=app
+set VDexe=\Github\Virtualdesktop\Virtualdesktop.exe
+start vlc --play-and-exit srt://%stream_ip%:1935?streamid=output/live/%stream_key%
+start %VDexe% -Remove:VLC
+start %VDexe% -New -Name:VLC
+start %VDexe% -Getdesktop:VLC -MoveWindowHandle:VLC
